f20a2d810111b0f1d01315e3c6e6658539e20075
Alex Andrix <alex@alexandrix.com>
2022-12-21 12:29:54 +0100

    Toggle mechanics for 3 variables

--- js/controls.js
+++ js/controls.js
@@ -6,20 +6,45 @@
  */
 
 let C = {
-	axisToggle: false
+	axisToggle: false,
+	gridToggle: false,
+	loglogToggle: false
 }
 
 document.addEventListener("DOMContentLoaded", (ev) => {
 	document.addEventListener("keyup", (event) => {
+		
+		let toggledVariable
+		
+		// 'A' key
 		if (event.keyCode == 65) {
 			// Toggle global control variable
 			C.axisToggle = !C.axisToggle
-			
-			// Apply to all instances of UI controls
-			Array.from(document.getElementsByClassName('key-icon')).forEach((el) => {
-				log(el)
-				
-			})
+			toggledVariable = C.axisToggle
 		}
+		
+		// 'G' key
+		if (event.keyCode == 71) {
+			// Toggle global control variable
+			C.gridToggle = !C.gridToggle
+			toggledVariable = C.gridToggle
+		}
+		
+		// 'L' key
+		if (event.keyCode == 76) {
+			// Toggle global control variable
+			C.loglogToggle = !C.loglogToggle
+			toggledVariable = C.loglogToggle
+		}
+		
+		// Apply to all instances of UI controls
+		Array.from(document.getElementsByClassName('key-icon')).forEach((el) => {
+			if (el.getAttribute('data-keycode') == event.keyCode) {
+				el.parentNode.classList.remove('active')
+				if (toggledVariable) el.parentNode.classList.add('active')
+			}
+		})
+		
+		
 	})
 })
